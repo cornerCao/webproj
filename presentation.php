4c8e19aea53d0cd7f6c6ffff5c04fd9e5aece243
cornerCao <852286848@qq.com>
2016-11-29 19:48:15 +0800

    Update presentation.php

--- presentation.php
+++ presentation.php
@@ -26,18 +26,14 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li><a href="./conference.php">Home</a></li>
+                <li ><a href="search.php">Search</a></li>
                 <li class="active"><a href="#">Presentations <span class="sr-only">(current)</span></a></li>
                 <li><a href="./speakers.php">Speakers</a></li>
                 <li><a href="./attractions.html">Attractions</a></li>
                 <li><a href="RouteFromHKAirport.php">Route</a></li>
                 <li><a href="#">About</a></li>
             </ul>
-            <form class="navbar-form navbar-right">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+        
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
