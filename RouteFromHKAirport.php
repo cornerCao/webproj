28b56da1121382dc351d09de0817a471076b4757
cornerCao <852286848@qq.com>
2016-11-29 19:50:15 +0800

    Update RouteFromHKAirport.php

--- RouteFromHKAirport.php
+++ RouteFromHKAirport.php
@@ -143,19 +143,15 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li><a href="conference.php">Home</span></a></li>
+          <li><a href="conference.php">Home</a></li>
+      <li><a href="search.php">Search</a></li>
           <li><a href="./presentation.php">Presentations</a></li>
           <li><a href="./speakers.php">Speakers</a></li>
           <li><a href="./attractions.html">Attractions</a></li>
           <li class="active"><a href="#">Route<span class="sr-only">(current)</span></a></li>
           <li><a href="#">About</a></li>
         </ul>
-        <form class="navbar-form navbar-right">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
+       
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
