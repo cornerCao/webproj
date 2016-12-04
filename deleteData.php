2a98aeb647d36bdf37e3de93bf093261b6296b7e
Anna <593880978@qq.com>
2016-12-04 23:49:23 +0800

    update

--- deleteData.php
+++ deleteData.php
@@ -2,13 +2,13 @@
 $connect=mysql_connect("mysql.comp.polyu.edu.hk","16027789x","jsiyppoo") or die("链接数据库失败！"); 
 //连接数据库(test) 
 mysql_select_db("16027789x",$connect) or die (mysql_error());
-$sql="TRUNCATE TABLE Conference;";
+$sql="TRUNCATE TABLE events;";
 mysql_query($sql) or die(mysql_error());
-$sql="TRUNCATE TABLE Speaker;";
+$sql="TRUNCATE TABLE presentation;";
 mysql_query($sql) or die(mysql_error());
-$sql="TRUNCATE TABLE Event;";
+$sql="TRUNCATE TABLE authorsabstracts;";
 mysql_query($sql) or die(mysql_error());
-$sql="TRUNCATE TABLE Presentation;";
-mysql_query($sql) or die(mysql_error());
+//$sql="TRUNCATE TABLE Presentation;";
+//mysql_query($sql) or die(mysql_error());
 echo "删除成功！";
 ?>
